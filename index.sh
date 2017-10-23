0e3d0270f865a12c50f0c34bbd363050689b0e6a
afeiship <1290657123@qq.com>
2017-10-23 14:20:18 +0800

    bugfix

--- index.sh
+++ index.sh
@@ -4,4 +4,4 @@ ROOT_PATH=$(dirname $BASH_SOURCE);
 
 alias network-down='sudo ifconfig en0 down';
 alias network-up='sudo ifconfig en0 up';
-alias network-restart='./restart.sh';
+alias network-restart="$ROOT_PATH/restart.sh";
